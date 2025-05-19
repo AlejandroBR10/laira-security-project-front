df4a4ab6d50114b410d84397ca16da03cd59df6e
alanuwu <nosaboxdxdjajasalu2@gmail.com>
2025-05-19 13:07:06 -0600

    feat: NavBar productos

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-
 export default {
-    server: {
-      host: '127.0.0.1', 
-      port: 3000,
-    },
-  };
+  server: {
+    host: '127.0.0.1',
+    port: 3000,
+    allowedHosts: ['generally-model-tarpon.ngrok-free.app']
+  },
+};
